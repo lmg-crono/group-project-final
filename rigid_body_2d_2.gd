3a7f7fcfbbc2e28f0ebcb5e680b26ce30f5ee7a8
lmg-crono <crono@louisvillemakesgames.org>
2026-07-22 16:39:23 -0400

    more edits
    jsdsdsadsadsa

--- rigid_body_2d_2.gd
+++ rigid_body_2d_2.gd
@@ -1,8 +1,12 @@
 extends RigidBody2D
-@export var push_force = 600
+@export var push_force = 50
+var pushed = false
 func _on_body_entered(body: Node) -> void:
+	if pushed: return
 	if body.name == "CharacterBody2D":
 		var push_direction = Vector2.RIGHT
 		if global_position.x < body.global.position.x:
 			push_direction = Vector2.LEFT
-		apply_force(push_force * push_direction)
+		apply_impulse(push_force * push_direction)
+		pushed = true
+		
