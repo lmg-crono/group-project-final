ca4bd4a16269ec542a28715804f9182265787961
lmg-crono <crono@louisvillemakesgames.org>
2026-07-23 16:22:26 -0400

    more stuff

--- rigid_body_2d_2.gd
+++ rigid_body_2d_2.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
-@export var push_force = 50
-var pushed = false
-func _on_body_entered(body: Node) -> void:
-	if pushed: return
-	if body.name == "CharacterBody2D":
-		var push_direction = Vector2.RIGHT
-		if global_position.x < body.global.position.x:
-			push_direction = Vector2.LEFT
-		apply_impulse(push_force * push_direction)
-		pushed = true
-		
-		
-		
-func _physics_process(delta: float) -> void:
-	print(linear_velocity)
+#@export var push_force = 50
+#var pushed = false
+#func _on_body_entered(body: Node) -> void:
+	#if pushed: return
+	#if body.name == "CharacterBody2D":
+		#var push_direction = Vector2.RIGHT
+		#if global_position.x < body.global.position.x:
+			#push_direction = Vector2.LEFT
+		#apply_impulse(push_force * push_direction)
+		#pushed = true
+		#
+		#
+		#
+#func _physics_process(delta: float) -> void:
+	#print(linear_velocity)
