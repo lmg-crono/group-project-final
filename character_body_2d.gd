cbdbb59242193fb364ea2b79e01af5aa866e9f7e
lmg-bub <bub@louisvillemakesgames.org>
2026-07-24 13:25:06 -0400

    fixed kick animation

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -11,7 +11,10 @@ func _physics_process(delta: float) -> void:
 	#this is where it moves back and forth
 	velocity.x = Input.get_axis("walk_left", "walk_right") * speed
 	
-	if abs(velocity.y) > 100:
+	if not $KickTimer.is_stopped():
+		$CharecterAnimation.play("kicking")
+	
+	elif abs(velocity.y) > 100:
 		$CharecterAnimation.play("jump")
 	elif  (velocity.x) > 0:
 		$CharecterAnimation.play("walking-right")
@@ -26,5 +29,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= 550
 	if Input.is_action_just_pressed("Kick"):
+		$KickTimer.start()
 		$CharecterAnimation.play("kicking")
 	move_and_slide()
