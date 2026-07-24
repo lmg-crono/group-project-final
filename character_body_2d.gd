23f3ac5eb2aa405c1b749e701b8c7fd407efb99c
lmg-crono <crono@louisvillemakesgames.org>
2026-07-24 15:43:04 -0400

    Please no issues

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,13 @@
 
 extends CharacterBody2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 
 @export var speed : int = 450
 @export var gravity : int = 800
 @export var jump_speed : int = 500
-
+var kickbox_offset 
+func _ready():
+	kickbox_offset = collision_shape_2d.position.x
 func _physics_process(delta: float) -> void:
 	#this  is gravity
 	velocity.y += gravity * delta
@@ -12,23 +15,41 @@ func _physics_process(delta: float) -> void:
 	velocity.x = Input.get_axis("walk_left", "walk_right") * speed
 	
 	if not $KickTimer.is_stopped():
-		$CharecterAnimation.play("kicking")
+		$CharacterAnimation.play("kicking")
 	
 	elif abs(velocity.y) > 100:
-		$CharecterAnimation.play("jump")
-	elif  (velocity.x) > 0:
-		$CharecterAnimation.play("walking-right")
+		$CharacterAnimation.play("jump")
+	elif  (velocity.x) != 0:
+		$CharacterAnimation.play("walking-right")
 		
-	elif (velocity.x) < 0:
-		$CharecterAnimation.play("walking-left ")
+	#elif (velocity.x) < 0:
+		#$CharacterAnimation.play("walking-left ")
 		
 	else:
-		$CharecterAnimation.play("idle")
+		$CharacterAnimation.play("idle")
 		
 	#this is where we jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= 550
 	if Input.is_action_just_pressed("Kick"):
 		$KickTimer.start()
-		$CharecterAnimation.play("kicking")
+		$CharacterAnimation.play("kicking")
+		if $CharacterAnimation.flip_h:
+			collision_shape_2d.position.x = -kickbox_offset
+		else: 
+			collision_shape_2d.position.x = kickbox_offset
+		collision_shape_2d.set_deferred("disabled",false)
 	move_and_slide()
+	if velocity.x > 0:
+		$CharacterAnimation.flip_h = false
+	elif velocity.x < 0:
+		$CharacterAnimation.flip_h = true
+
+
+func _on_kick_timer_timeout() -> void:
+	collision_shape_2d.set_deferred("disabled",true)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"): 
+		body.queue_free()
--- screenshot_20260722124751.gd
+++ screenshot_20260722124751.gd
@@ -1,5 +1,5 @@
 extends StaticBody2D
 #walk into secret area door to destroy it, eventually replace for kick.
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "CharacterBody2D":
+	if body.is_in_group("kick") :
 		queue_free()
